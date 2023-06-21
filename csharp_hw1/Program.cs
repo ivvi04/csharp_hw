b4f749c98daae80939ec4214750e4d6f5a97448d
LakeevDA <ivvi04@gmail.com>
2023-06-21 17:54:44 +0300

    Добавлена домашняя работа по уроку 3

--- csharp_hw1/Program.cs
+++ csharp_hw1/Program.cs
@@ -43,8 +43,8 @@ if (a % 2==1) {
 // ------------------------------------------------------------------------
 // Task6
 Console.WriteLine("Введите число");
-int a = Convert.ToInt32(Console.ReadLine());
-int b = 1;
+a = Convert.ToInt32(Console.ReadLine());
+b = 1;
 
 while (b <= a) {
     if (b % 2!=1) {
