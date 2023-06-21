615e4dc47a79407f7ec8edca9ab5cecb8fc1de37
LakeevDA <ivvi04@gmail.com>
2023-06-21 17:23:53 +0300

    Добавлены домашние задания 1 и 2

---
+++ csharp_hw1/Program.cs
@@ -0,0 +1,54 @@
+﻿// Task2
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a < b) {
+    Console.WriteLine("min = " + a.ToString() + ", max = " + b.ToString());
+} else {
+    Console.WriteLine("min = " + b.ToString() + ", max = " + a.ToString());
+}
+
+// ------------------------------------------------------------------------
+// Task4
+Console.WriteLine("Введите первое число");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (max < b) {
+    max = b;
+}
+if (max < c) {
+    max = c;
+}
+
+Console.WriteLine("max = " + max.ToString());
+
+// ------------------------------------------------------------------------
+// Task6
+Console.WriteLine("Введите число");
+a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2==1) {
+    Console.WriteLine("число нечетное");
+} else {
+    Console.WriteLine("Число четное");
+}
+
+// ------------------------------------------------------------------------
+// Task6
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 1;
+
+while (b <= a) {
+    if (b % 2!=1) {
+        Console.Write(b.ToString() + " ");
+    }
+    b = b + 1;
+}
---
+++ csharp_hw2/Program.cs
@@ -0,0 +1,69 @@
+﻿void Task10 () {
+    Console.WriteLine("Выбрано задание 10");
+    Console.Write("Введите трехзначное число: ");
+    
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    if (number.ToString().Length == 3) {
+        //Console.WriteLine("Вторая цифра: " + number.ToString()[1]);
+        Console.WriteLine("Вторая цифра: " + ((number / 10) % 10).ToString());
+    } else {
+        Console.WriteLine("Введено не трехзначное число!");
+    }
+}
+
+int Get3ndDigit(int number) {
+    while (number >= 1000) number /= 10;
+    while (number > 10) number %= 10;
+    return number;
+}
+
+void Task13 () {
+    Console.WriteLine("Выбрано задание 13");
+    Console.Write("Введите число: ");
+    
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    if (number.ToString().Length >= 3) {
+        //Console.WriteLine("Третья цифра: " + number.ToString()[2]);
+        Console.WriteLine("Третья цифра: " + Get3ndDigit(number).ToString());
+    } else {
+        Console.WriteLine("Третьей цифры нет!");
+    }
+}
+
+void Task15 () {
+    Console.WriteLine("Выбрано задание 15");
+    Console.Write("Введите цифру от 1 до 7: ");
+    
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    if (number >= 1 && number <= 7) {
+        if (number > 5) {
+            Console.WriteLine("Выходной день");
+        } else {
+            Console.WriteLine("Не выходной день");
+        }
+    } else {
+        Console.WriteLine("Введена другая цифра или число!");
+    }
+}
+
+Console.Write("Выбирете задание (10, 13, 15): ");
+int task = Convert.ToInt32(Console.ReadLine());
+
+switch (task) {
+    case 10:
+        Task10();
+        break;
+    case 13:
+        Task13();
+        break;
+    case 15:
+        Task15();
+        break;
+    default:
+        Console.WriteLine("Выбрано неверный номер задания");
+        break;
+}
+
